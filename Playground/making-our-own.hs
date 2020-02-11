939ffebaf8c7c826890f5637f8292916a98e7e85
Sumit Mukhija <sumitmukhija@hotmail.com>
2020-02-11 11:52:39 +0000

    Working towards custom data and typeclasses

---
+++ Playground/making-our-own.hs
@@ -0,0 +1,32 @@
+-- making-our-own
+
+data Center = Point Int Int deriving (Show)
+-- data Radius = Radius Float deriving (Show)
+data Shape = Circle Center Float | Rectangle Int Int Int Int deriving (Show)
+
+
+area:: Shape -> Float
+area (Circle _ r) = 3.14 * r * r
+area (Rectangle a b c d) = 1
+
+-- map with value constructors
+diffCircles = map (Circle (Point 2 2)) [4,5,6,6]  
+
+
+-- Person
+data Person = Person String String Int Bool Car deriving (Show)
+data Car = Car String Int deriving (Show)
+
+chevy = Car "Chevy" 1990
+jon = Person "Jon" "Galway" 32 True chevy
+
+
+nameAndPlace:: Person -> String
+nameAndPlace (Person name city _ _ _) = name ++ " from "++ city
+
+makeAndYear:: Car -> String
+makeAndYear (Car name year) = name ++ " of " ++ show year
+
+whatCar:: Person -> String
+whatCar (Person _ _ _ _ car) = (makeAndYear car)
+
