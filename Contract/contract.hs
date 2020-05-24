3ba2c3ac878a1d48f68e999a302a8d18a1972164
Sumit Mukhija <sumitmukhija@hotmail.com>
2020-05-24 18:26:24 +0100

    1. First line that defines module 2. File name changed from contract.hs to Contract.hs 3. Removed _ from function names 4. Swapping sender and receiver in give 5. Breaking down lines 6. Constructors for contract lang

--- Contract/contract.hs
+++ Contract/contract.hs
@@ -1,71 +1,80 @@
-data Contract = Contract {acqDate:: Int, expDate:: Int, amount:: Double, currency:: [Char], sender:: Char, receiver:: Char} | Zero | One [Char] | Give Contract | And Contract Contract | Or Contract Contract | Truncate Int Contract deriving Show
+module Contract where
+data Contract = Contract {acqDate:: Int, expDate:: Int, amt:: Double, curr:: [Char], sen:: Char, rec:: Char} 
+				| Zero 
+				| One [Char] 
+				| Give Contract 
+				| And Contract Contract 
+				| Or Contract Contract 
+				| Truncate Int Contract deriving Show
 
--- contract with no rights, obligations
--- infinite horizon
-_zero = Zero
+
+-- Constructors
+Zero = Contract 0 0 0 [] 'A' 'B'
+One c = Contract 0 (-1) 1 c 'A' 'B'
+Give (Contract acq e a cr s r) = Contract acq e a cr r s
+And cn1 cn2 = let 
+	curr = if areCurrenciesConsistent cn1 cn2 then currency cn1 else "Error"
+	amt = sumOfAmountsFromContract cn1 cn2
+	edt = laterExpiryDateFromContract cn1 cn2
+	adt = soonerAcquiringDateFromContract cn1 cn2
+	sen = sender cn1
+	recv = receiver cn1
+	in Contract adt edt amt curr sen recv
+
+-- Wrapper functions
+-- contract with no rights, obligations, infinite horizon
+zero = Zero
 
 -- immediately pays 1 unit. Infinite horizon. 0 as first argument defines "now"
 -- -1 as second arg defines infinite expiry
-_one:: [Char] -> Contract
-_one currency = Contract 0 (-1) 1 currency 'A' 'B'
+one:: [Char] -> Contract
+one curr = One curr
 
 -- Reverses role. Could have simply changed the amount to negative, too.
-_give:: Contract -> Contract
-_give (Contract aTimestamp eTimestamp amount currency sender receiver) = 
-	let
-	newAmount = (-amount)
-	in 
-	Contract aTimestamp eTimestamp newAmount currency sender receiver
+give:: Contract -> Contract
+give con = Give con
 
 -- acquire both contracts. For the composites, adding amount, deciding latest date
-_and:: Contract -> Contract -> Contract
-_and c1 c2 =
-	let 
-	curr = if _areCurrenciesConsistent c1 c2 then _currency c1 else "Error"
-	amt = _sumOfAmountsFromContract c1 c2
-	edt = _laterExpiryDateFromContract c1 c2
-	adt = _soonerAcquiringDateFromContract c1 c2
-	sen = _sender c1
-	rec = _receiver c1
-	in Contract adt edt amt curr sen rec
+and':: Contract -> Contract -> Contract
+and' c1 c2 = And c1 c2
 
 -- either of the two contracts. If acquire date of one is in past, take the second
-_or:: Contract -> Contract -> Contract
-_or c1 c2 = 
+or:: Contract -> Contract -> Contract
+or c1 c2 = 
 	let 
-	contract = if _isInPast (_acquireDate c1)
+	contract = if isInPast (acquireDate c1)
 		then c2
 		else c1
 	in 
 	contract
 
 -- expires at given date
-_truncate:: Int -> Contract -> Contract
-_truncate date c1  = Contract (_acquireDate c1) date (_amount c1) (_currency c1) (_sender c1) (_receiver c1)
+truncate:: Int -> Contract -> Contract
+truncate date c1  = Contract (acquireDate c1) date (amount c1) (currency c1) (sender c1) (receiver c1)
 
 
-_then:: Contract -> Contract -> Contract
-_then c1 c2=
+then':: Contract -> Contract -> Contract
+then' c1 c2=
 	let 
-	contract = if _isInPast (_expiryDate c1)
+	contract = if isInPast (expiryDate c1)
 		then c2
 		else c1
 	in 
 	contract
 
 -- TODO://
-_scale:: Double -> Contract -> Contract
-_scale amt contract = c1
+scale:: Double -> Contract -> Contract
+scale amt contract = c1
 
 -- Acquire at expiry
-_get:: Contract -> Contract
-_get c = Contract (_expiryDate c) (_expiryDate c) (_amount c) (_currency c) (_sender c) (_receiver c)
+get:: Contract -> Contract
+get c = Contract (expiryDate c) (expiryDate c) (amount c) (currency c) (sender c) (receiver c)
 
 -- Contract to be procured between acquire date and expiry date
-_anytime:: Contract -> Contract
-_anytime c = 
+anytime:: Contract -> Contract
+anytime c = 
 	let 
-	contract = if (_acquireDate c < _now && _expiryDate c > _now) then _get c else Zero
+	contract = if (acquireDate c < now && expiryDate c > now) then get c else Zero
 	in contract
 
 
@@ -74,52 +83,53 @@ _anytime c =
 -- Checks for the timestamp to be in the past or future.
 
 -- represents current time simulation in epochs
-_now = 1500000000
+now = 1500000000
 
 -- Determines if a timestamp is in past or not.
-_isInPast:: Int -> Bool
-_isInPast date = date < _now
+isInPast:: Int -> Bool
+isInPast date = date < now
 
 -- Returns the acquire date from the contract
-_acquireDate:: Contract -> Int
-_acquireDate (Contract acqDate _ _ _ _ _) = acqDate
+acquireDate:: Contract -> Int
+acquireDate (Contract acqDate _ _ _ _ _) = acqDate
 
 -- Returns the expire date from the contract
-_expiryDate:: Contract -> Int
-_expiryDate (Contract _ expDate _ _ _ _) = expDate
+expiryDate:: Contract -> Int
+expiryDate (Contract _ expDate _ _ _ _) = expDate
 
 -- Adds the amount from two contracts
-_sumOfAmountsFromContract:: Contract -> Contract -> Double
-_sumOfAmountsFromContract (Contract _ _ amount1 _ _ _ ) (Contract _ _ amount2 _ _ _ )  = amount1 + amount2
+sumOfAmountsFromContract:: Contract -> Contract -> Double
+sumOfAmountsFromContract (Contract _ _ amt1 _ _ _ ) (Contract _ _ amt2 _ _ _)  = amt1 + amt2
 
 -- Checks and returns the later expiry date from the two contracts
-_laterExpiryDateFromContract:: Contract -> Contract -> Int
-_laterExpiryDateFromContract (Contract _ expiry_first _ _ _ _) (Contract _ expiry_second _ _ _ _) = if expiry_first > expiry_second then expiry_first else expiry_second
+laterExpiryDateFromContract:: Contract -> Contract -> Int
+laterExpiryDateFromContract (Contract _ expiryfirst _ _ _ _ ) (Contract _ expirysecond _ _ _ _) 
+	= if expiryfirst > expirysecond then expiryfirst else expirysecond
 
 -- Checks and returns the sooner acquiring date from the two contracts
-_soonerAcquiringDateFromContract:: Contract -> Contract -> Int
-_soonerAcquiringDateFromContract (Contract acquiring1 _ _ _ _ _) (Contract acquiring2 _ _ _ _ _) = if acquiring1 > acquiring2 then acquiring2 else acquiring1
+soonerAcquiringDateFromContract:: Contract -> Contract -> Int
+soonerAcquiringDateFromContract (Contract acquiring1 _ _ _ _ _) (Contract acquiring2 _ _ _ _ _ ) 
+	= if acquiring1 > acquiring2 then acquiring2 else acquiring1
 
 -- Checks if the currencies in the two contracts are same
-_areCurrenciesConsistent:: Contract -> Contract -> Bool
-_areCurrenciesConsistent (Contract _ _ _ c1 _ _) (Contract _ _ _ c2 _ _) = c1 == c2
+areCurrenciesConsistent:: Contract -> Contract -> Bool
+areCurrenciesConsistent (Contract _ _ _ c1 _ _ ) (Contract  _ _ _ c2 _ _) = c1 == c2
 
 -- Gets amount from the contract
-_amount:: Contract -> Double
-_amount (Contract _ _ amount _ _ _) = amount
+amount:: Contract -> Double
+amount (Contract  _ _ amt _ _ _ ) = amt
 
 -- Gets sender from the contract
-_sender:: Contract -> Char
-_sender (Contract _ _ _ _ sender _) = sender
+sender:: Contract -> Char
+sender (Contract _ _ _ _ sen _) = sen
 
 -- Gets receiver from the contract
-_receiver:: Contract -> Char
-_receiver (Contract _ _ _ _ _ receiver) = receiver
+receiver:: Contract -> Char
+receiver (Contract _ _ _ _ _ rece) = rece
 
 -- Gets currency from a contract
-_currency:: Contract -> [Char]
-_currency (Contract _ _ _ currency _ _) = currency
-
+currency:: Contract -> [Char]
+currency (Contract  _ _ _ currency _ _) = currency
 
 -- Sample Contracts 
 
