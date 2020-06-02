fe89d77c965f5ded1d5e5986ab09435c7c4d4bc6
sumit mukhija <sumukhija@gmail.com>
2020-06-02 22:16:18 +0100

    Change GenericContract to Contract in data Contract

--- Contract/contract.hs
+++ Contract/contract.hs
@@ -2,51 +2,51 @@ module Contract where
     data GenericContract = GenericContract Int Int Double [Char] Char Char deriving Show
     data Contract = Zero
                     | One [Char]
-                    | Give GenericContract
-                    | And GenericContract GenericContract
-                    | Truncate Int GenericContract deriving Show
+                    | Give Contract
+                    | And Contract Contract deriving Show
 
 
-    getGenericContractFromContract:: Contract -> GenericContract
-    getGenericContractFromContract Zero = GenericContract 0 0 0 [] 'A' 'B'
-    getGenericContractFromContract (One currency) = GenericContract 0 (-1) 1 currency 'A' 'B'
-    getGenericContractFromContract (Give genericContract) = genericContract
-    getGenericContractFromContract (And gc1 gc2) = let a = soonerAcquiringDateFromContract gc1 gc2
-                                                       e = laterExpiryDateFromContract gc1 gc2
-                                                       am = sumOfAmountsFromGenericContracts gc1 gc2
-                                                       cr = currency gc1
-                                                       s =  sender gc1
-                                                       r = receiver gc1 in 
-                                                           GenericContract a e am cr s r
-
-    give:: GenericContract -> Contract
-    give (GenericContract acq e a cr s r) = Give (GenericContract acq e a cr r s)
-
-    -- Utility fns
-    sumOfAmountsFromGenericContracts (GenericContract _ _ a1 _ _ _) (GenericContract _ _ a2 _ _ _) = a1+a2
+    -- Function that converts contract to generic contract
+    genericContractFromContract:: Contract -> GenericContract
+    genericContractFromContract Zero = GenericContract 0 0 0 [] 'A' 'B'
+    genericContractFromContract (One currency) = GenericContract 0 0 1 currency 'A' 'B'
+    genericContractFromContract (Give contract) = give(genericContractFromContract contract)
+    genericContractFromContract (And c1 c2) = 
+                            and' (genericContractFromContract c1) (genericContractFromContract c2)
     
-    -- Checks and returns the later expiry date from the two contracts
+    -- implementation methods with generic contracts
+    and' gc1 gc2 = let 
+                     a = soonerAcquiringDateFromContract gc1 gc2
+                     e = laterExpiryDateFromContract gc1 gc2
+                     amt = sumOfAmounts gc1 gc2
+                     curr = currency gc1
+                     s = sender gc1
+                     r = receiver gc1
+                     in GenericContract a e amt curr s r
+
+    give (GenericContract a e amt c s1 r1) =  GenericContract a e amt c r1 s1
+
+
+    -- Utility methods
     laterExpiryDateFromContract:: GenericContract -> GenericContract -> Int
     laterExpiryDateFromContract (GenericContract _ expiryfirst _ _ _ _ ) (GenericContract _ expirysecond _ _ _ _) = 
         if expiryfirst > expirysecond then expiryfirst else expirysecond
 
-    -- Checks and returns the sooner acquiring date from the two contracts
     soonerAcquiringDateFromContract:: GenericContract -> GenericContract -> Int
-    soonerAcquiringDateFromContract (GenericContract acquiring1 _ _ _ _ _) (GenericContract acquiring2 _ _ _ _ _ ) =
-         if acquiring1 > acquiring2 then acquiring2 else acquiring1
+    soonerAcquiringDateFromContract (GenericContract acquiringFirst _ _ _ _ _) (GenericContract acquiringSecond _ _ _ _ _ ) =
+         if acquiringFirst > acquiringSecond then acquiringSecond else acquiringFirst
 
-    -- Gets currency from a generic contract
-    currency:: GenericContract -> [Char]
-    currency (GenericContract  _ _ _ currency _ _) = currency
+    sumOfAmounts (GenericContract _ _ amountFirst _ _ _) (GenericContract _ _ amountSecond _ _ _ ) = amountFirst + amountSecond
     
-    -- Gets sender from the generic contract
-    sender:: GenericContract -> Char
-    sender (GenericContract _ _ _ _ sen _) = sen
+    -- Getters
+    currency (GenericContract _ _ _ c _ _) = c
 
-    -- Gets receiver from the  generic contract
-    receiver:: GenericContract -> Char
-    receiver (GenericContract _ _ _ _ _ rece) = rece
+    sender (GenericContract _ _ _ _ s _) = s
     
-    -- Sample generic contracts
-    c1 = GenericContract 1590420037 1590410021 100.00 "EUR" 'A' 'B'
-    c2 = GenericContract 1653492037 1853221037 121.50 "EUR" 'B' 'C'
+    receiver (GenericContract _ _ _ _ _ r) = r
+    
+    
+    -- Test contracts
+    o = One "GBP"
+    z = Zero
+    o' = One "GBP"
