1094572447fe81421c1a4e416b533c4615176b92
sumit mukhija <sumitmukhija@hotmail.com>
2020-07-14 16:58:15 +0100

    Add EdgeAttributeType to module Edge. Refactor needed

--- project/src/dsl/edge/Edge.hs
+++ project/src/dsl/edge/Edge.hs
@@ -1,15 +1,27 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Edge where
     
     import Vertex
     import Utils
-    import Alias
 
-    class VertexType v => EdgeType e v | v -> e where
-        edge:: v -> v -> e
+    type EdgeIdentifier = String
+    type EdgeAttributes = [EdgeAttribute]
+
+    class VertexType v => EdgeType e v where
+        edge:: v -> v -> EdgeAttributes -> e
     
-    data Edge = Edge Vertex Vertex EdgeIdentifier deriving Show
+    class EdgeAttributeType eat e where
+        setEdgeAttribute :: e -> eat -> e
+        getEdgeAttributes :: e -> [eat]
+
+    data Edge = Edge Vertex Vertex EdgeIdentifier EdgeAttributes deriving Show
+    data EdgeAttribute = EdgeAttribute String deriving Show
 
     instance EdgeType Edge Vertex where 
-        edge vx vy = Edge vx vy (edgeIdentifierFromVertices vx vy)
+        edge vx vy attributes = Edge vx vy (edgeIdentifierFromVertices vx vy) attributes
         
+    instance EdgeAttributeType EdgeAttribute Edge where
+        getEdgeAttributes (Edge _ _ _ attrs) = attrs
+        setEdgeAttribute (Edge v1 v2 _ attrs) attr = 
+            (edge v1 v2 (attrs ++ [attr]))
+    
